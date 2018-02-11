54108cdebf95825210166b3bcf78234fc4e9e5ac
Brad Carlson <b2carlso@yahoo.com>
2018-02-11 13:31:24 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,4 +19,27 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, ingredient_ids: [])
+    end
+
 end
